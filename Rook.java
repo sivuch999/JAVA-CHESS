30f80cd97e3b51dcb9b51c84051e68d57abe7a84
sivuch999 <slowsoup@hotmail.com>
2019-10-19 23:16:26 +0700

    Rook 100%

--- Rook.java
+++ Rook.java
@@ -1,15 +1,15 @@
 
 public class Rook extends ChessPiece {
 
-	public Rook(int row, int col,Player player) {
-		super(row, col,player);
+	public Rook(int row, int col, Player player) {
+		super(row, col, player);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	void printPiece() {
 		System.out.print((this.player.number == 0) ? "r " : "R ");
-		
+
 	}
 
 	@Override
@@ -19,12 +19,37 @@ public class Rook extends ChessPiece {
 
 	@Override
 	boolean isValidMove(ChessBoard board, int row, int col) {
-		if(this.row != row && this.col != col) {
-			System.out.println("Can't Move!");
-			return false;
+		ChessPiece pieceAt = null;
+		if(
+			row < 0 || col < 0 
+			|| row > board.BOARD_SIZE || col > board.BOARD_SIZE
+			|| (this.row == row && this.col == col)
+		){ return false; 
+		}else{
+			if(this.row == row && this.col != col) {
+				Integer i = this.col;
+				if(this.col < col) { i = ((this.col + 1 <= 7) ? this.col + 1 : this.col ); }
+				else{ i = ((this.col - 1 >= 0) ? this.col - 1 : this.col ); }
+				while (i != ((this.col < col) ? col + 1 : col - 1 )) {
+					pieceAt = board.pieceAt(row, i);
+					if(pieceAt != null){ return false; }
+					if(this.col < col){ i++; }else{ i--; }
+				}
+				return pieceAt != null ? this.player != pieceAt.player : true;
+			} else if (this.col == col && this.row != row) {
+				Integer i = 0;
+				if(this.row < row) { i = ((this.row + 1 <= 7) ? this.row + 1 : this.row ); }
+				else { i = ((this.row - 1 >= 0) ? this.row - 1  : this.row ); }
+				while (i != ((this.row < row) ? row + 1 : row - 1 )) {
+					pieceAt = board.pieceAt(i, col);
+					System.out.println(pieceAt);
+					if(pieceAt != null){ return false; }
+					if(this.row < row){ i++; }else{ i--; }
+				}
+				return pieceAt != null ? this.player != pieceAt.player : true;
+			}else{ return false; }
 		}
-		return true;
+
 	}
-	
-	
+
 }
